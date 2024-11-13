2e390d3c8afe731646ba6945bc013daf5d4e81fe
Yuri Melo Nunes <ymelonunes@gmail.com>
2024-11-12 21:11:37 -0300

    input data adjustment

--- cvrp_gnn.py
+++ cvrp_gnn.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import vrplib
+import numpy as np
 
 def le_instancia_solucao(instancia, solucao):
     
@@ -28,7 +29,7 @@ def le_instancia_solucao(instancia, solucao):
 #Programa para tratamento das variáveis: https://github.com/sudhan-bhattarai/
 #   CVRPTW_MILP/blob/master/CVRPTW.py
 
-def le_cvrptw(instancia:str, solucao:str):    
+def le_cvrptw(instancia, solucao):    
             
     nome_da_instancia = instancia['name']
     comentario_instancia = instancia['comment']
@@ -78,79 +79,156 @@ def le_cvrptw(instancia:str, solucao:str):
     
     
 #------------------------------------------------------------------------------
-import sys
-instancia = ("./data/Vrp-Set-XML100/instances/XML100_1111_01.vrp",
-             "./data/Vrp-Set-XML100/solutions/XML100_1111_01.sol")
-instancia, solucao = le_instancia_solucao(instancia[0], instancia[1])
-                                          
-x = le_cvrptw(instancia, solucao)
+import os
+dados_entrada_gnn = []
+diretorio_inst = "./data/Vrp-Set-XML100/instances/"
+lista_arquivos = os.listdir(diretorio_inst)
 
-# A partir deste ponto serão gerados todos os dados necessários à Rede Neural.
-nome_da_instancia = instancia['name']
-comentario_instancia = instancia['comment']
-tipo_instancia = instancia['type']
-dimensao_instancia = instancia['dimension']
-tipo_custo = instancia['edge_weight_type']
-capacidade_veiculo = instancia['capacity']
-nos_coordenadas = instancia['node_coord']
-nos_demanda = instancia['demand']
-deposito_instancia = instancia['depot']
-peso_arcos = instancia["edge_weight"]
+#############################
+for i in range(len(lista_arquivos)):
+    inst =  diretorio_inst + lista_arquivos[i]
+    instancia = vrplib.read_instance(inst)
 
-rotas_da_solucao = solucao['routes']
-custo_da_solucao = solucao['cost']
+    # A partir deste ponto serão gerados todos os dados necessários à Rede Neural.
+    nome_da_instancia = instancia['name']
+    comentario_instancia = instancia['comment']
+    tipo_instancia = instancia['type']
+    dimensao_instancia = instancia['dimension']
+    tipo_custo = instancia['edge_weight_type']
+    capacidade_veiculo = instancia['capacity']
+    nos_coordenadas = instancia['node_coord']
+    nos_demanda = instancia['demand']
+    deposito_instancia = instancia['depot']
+    peso_arcos = instancia["edge_weight"]
+    
+    #rotas_da_solucao = solucao['routes']
+    #custo_da_solucao = solucao['cost']
+    
+    # 0) Cria tupla para instancia corrente:
+    tupla_entrada = ([], [], [], capacidade_veiculo)
+    #tupla_entrada = [[], [], [], capacidade_veiculo]
+    
+    # 1) Geração de features dos nós:
+    # 1.1) Feature coordenadas geográficas (distância Euclidiana)
+    # do depot e de todos os nós:
+    grid_size = 1
+    # normalização das coordenadas x e y
+    x_normalizado = nos_coordenadas[0][0] / grid_size
+    y_normalizado = nos_coordenadas[0][1] / grid_size
+    tupla_entrada[0].append(x_normalizado)
+    tupla_entrada[0].append(y_normalizado)
+    
+    # normalização das coordenadas x e y
+    for i in range(1, dimensao_instancia):
+        no = []
+        tupla_entrada[1].append(no)
+        x_normalizado = nos_coordenadas[i][0] / grid_size
+        y_normalizado = nos_coordenadas[i][1] / grid_size
+        tupla_entrada[1][i - 1].append(x_normalizado)
+        tupla_entrada[1][i - 1].append(y_normalizado)
+    # 1.2) Feature carga a ser retirada no nó
+    # este item não precisa de normalização
+    for i in range(1, dimensao_instancia):
+        cap = nos_demanda[i]
+        tupla_entrada[2].append(cap)
+    
+    # 2) Geração da informação da capacidade do caminhão. Esta
+    # informação não é normalizada. Foi gerada na criação da tupla.
+    # capacidade_veiculo = instancia['capacity']
+    
+    dados_entrada_gnn.append(tupla_entrada)
+    del(tupla_entrada)
 
-# 1) Geração de features dos nós:
-# 1.1) Feature coordenadas geográficas (distância Euclidiana)
-maior = 0
-for i in range(dimensao_instancia):
-    if nos_coordenadas[i][0] > maior:
-        maior = nos_coordenadas[i][0]
-    if nos_coordenadas[i][1] > maior:
-        maior = nos_coordenadas[i][1]
- # normalização das coordenadas x e y
-feature_coor = []
-for i in range(dimensao_instancia):
-    no = []
-    x = nos_coordenadas[i][0] / maior
-    y = nos_coordenadas[i][1] / maior
-    no.append(x)
-    no.append(y)
-    feature_coor.append(no)
 
-# 1.2) Feature carga a ser retirada no nó
-# normalizando pela capacidade do veículo
-feature_capac = []
-for i in range(dimensao_instancia):
-    cap = nos_demanda[i] / capacidade_veiculo
-    feature_capac.append(cap)
+entrada_gnn_numpy = np.array(dados_entrada_gnn, dtype=object)
+# Imprime dados de instâncias
+"""
+for i in range(len(lista_arquivos)):
+    #print(dados_entrada_gnn[i])
+    print(entrada_gnn_numpy[i])
+    #print(lista_arquivos[i])
+    print()
+"""
+# Suponha que array_dados já contenha seus dados
+n_total = len(entrada_gnn_numpy)
 
-# 2) Geração do mapa geral de distância entre todos os nós.
-mapa_coord = []
-for i in range(dimensao_instancia):
-    linha = []
-    for j in range(dimensao_instancia):
-        dist_arco = peso_arcos[i][j] / maior
-        linha.append(dist_arco)
-    mapa_coord.append(linha)
+# Índices para dividir os dados
+train_end = int(0.7 * n_total)     # 70%
+val_end = int(0.9 * n_total)       # 90%
 
-# 3) Valor ótimo do custo:
-custo = custo_da_solucao / maior
+# Dividir os dados
+dados_treino = entrada_gnn_numpy[:train_end]
+dados_validacao = entrada_gnn_numpy[train_end:val_end]
+dados_teste = entrada_gnn_numpy[val_end:]
 
-# 4) Dados de saída:
-heatmap = []
-for i in range(dimensao_instancia):
-    linha = []
-    for j in range(dimensao_instancia):
-        linha.append(0)
-    heatmap.append(linha)
-cont = 0
-for i in range(len(rotas_da_solucao)):
-    anterior = 0
-    for j in range(len(rotas_da_solucao[i])):
-        heatmap[anterior][rotas_da_solucao[i][j]] = 1
-        anterior = rotas_da_solucao[i][j]
-    heatmap[anterior][0] = 1
+# Salvar cada parte em arquivos .npy
+np.save('dados_treino.npy', dados_treino)
+np.save('dados_validacao.npy', dados_validacao)
+np.save('dados_teste.npy', dados_teste)
+
+print("Dados salvos em arquivos .npy com sucesso!")
+
+
+# A partir daqui vai gerar o arquivo com todas as soluções possíveis:
+dados_saida_gnn = []
+diretorio_solutions = "./data/Vrp-Set-XML100/solutions/"
+lista_arquivos = os.listdir(diretorio_solutions)
+for i in range(len(lista_arquivos)):
+    sol = diretorio_solutions + lista_arquivos[i]
+    solucao = vrplib.read_solution(sol)
+
+    # A partir deste ponto serão gerados todos os dados necessários à Rede Neural.
+   
+    rotas_da_solucao = solucao['routes']
+    #custo_da_solucao = solucao['cost']
+    
+    # 0) Cria lista para instancia corrente:
+    lista_saida = []
+    
+    # 1) Geração das rotas dos nós:
+    for i in range(len(rotas_da_solucao)):
+        rotas_da_solucao = solucao['routes']
+        for j in range(len(rotas_da_solucao[i])):
+            lista_saida.append(rotas_da_solucao[i][j])
+        if i < len(rotas_da_solucao) - 1:
+            lista_saida.append(0)
+    dados_saida_gnn.append(lista_saida)
+    del(lista_saida)
+
+
+dados_saida_gnn_numpy = np.array(dados_saida_gnn, dtype=object)
+    
+"""
+for i in range(len(lista_arquivos)):
+    print(dados_saida_gnn[i])
+    print(lista_arquivos[i])
+    print()
+"""
+
+# Suponha que array_dados já contenha seus dados
+n_total = len(dados_saida_gnn_numpy)
+
+# Índices para dividir os dados
+train_end = int(0.7 * n_total)     # 70%
+val_end = int(0.9 * n_total)       # 90%
+
+# Dividir os dados
+dados_solucao_treino = dados_saida_gnn_numpy[:train_end]
+dados_solucao_validacao = dados_saida_gnn_numpy[train_end:val_end]
+dados_solucao_teste = dados_saida_gnn_numpy[val_end:]
+
+# Salvar cada parte em arquivos .npy
+np.save('dados_solucao_treino.npy', dados_solucao_treino)
+np.save('dados_solucao_validacao.npy', dados_solucao_validacao)
+np.save('dados_solucao_teste.npy', dados_solucao_teste)
+
+print("Dados salvos em arquivos .npy com sucesso!")
+
+
+
+
+
+
 
 # Imprime heatmap:
 '''
